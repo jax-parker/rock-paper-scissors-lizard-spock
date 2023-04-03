7f8675344e19e4e01849ebd7d745ed83b1e9b062
jax-parker <parkyjax711@gmail.com>
2023-04-03 11:00:40 +0000

    add switch to check choices and console log message

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,7 +17,7 @@ const computerScore = document.getElementById('computer-score');
 const playerImage = document.getElementById('player-img');
 const compImage = document.getElementById('comp-img');
 const result = document.getElementById('result');
-const choices = ['rock','paper','scissors','lizard','spock'];
+const choices = ['rock', 'paper', 'scissors', 'lizard', 'spock'];
 let buttons = document.getElementsByTagName('button');
 let playerChoice;
 
@@ -25,23 +25,54 @@ let playerChoice;
 
 // Wait for the DOM to finish loading before running the game
 // Add event listeners to buttons 
-document.addEventListener("DOMContentLoaded", function(){
-for (button of buttons) {
-    button.addEventListener('click', function(){
-        playerChoice = this.getAttribute('data-type');
-        playGame(playerChoice);
-    });
-}
+document.addEventListener("DOMContentLoaded", function () {
+    for (button of buttons) {
+        button.addEventListener('click', function () {
+            playerChoice = this.getAttribute('data-type');
+            playGame(playerChoice);
+        });
+    }
 });
 
 function playGame(playerChoice) {
     const computerChoice = compChoice();
-    console.log('computer choice =>'  + compChoice())
-    console.log('player choice =>'  + playerChoice)
+    switch (playerChoice + computerChoice) {
+        case "paperrock":
+        case "paperspock":
+        case "scissorspaper":
+        case "scissorslizard":
+        case "rockscissors":
+        case "rocklizard":
+        case "lizardpaper":
+        case "lizardspock":
+        case "spockrock":
+        case "spockscissors":
+            console.log('player wins');
+            break;
+        case "rockpaper":
+        case "spockpaper":
+        case "paperscissors":
+        case "lizardscissors":
+        case "scissorsrock":
+        case "lizardrock":
+        case "paperlizard":
+        case "rockspock":
+        case "scissorsspock":
+            console.log('computer wins');
+            break;
+        case "rockrock":
+        case "paperpaper":
+        case "scissorsscissors":
+        case "lizardlizard":
+        case "spockspock":
+            console.log("It's a tie");
+            break;
+    }
+
 }
 
 function compChoice() {
-    const randomChoice=(Math.floor(Math.random()*5));
+    const randomChoice = (Math.floor(Math.random() * 5));
     return choices[randomChoice];
 }
 
